2786b439423eee93a2567ee93b6ce3413f8a4384
Don Guiruela <guirueladon@gmail.com>
2021-05-15 21:57:49 +0800

    .

---
+++ Aficio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+
+namespace Aficio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable data = new DataTable();
+            data.Columns.Add("col1");
+            data.Columns.Add("col2");
+            data.Columns.Add("col3");
+            data.Columns.Add("col4");
+
+            data.Rows.Add(new string[] { "Addition", "Subtraction", "Multiplication", "Division" });
+            data.Rows.Add(new string[] { "Science", "Mathematics", "Language", "Arts" });
+
+            string[] oof = data.GetCSV();
+
+            foreach (var a in oof)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
+
