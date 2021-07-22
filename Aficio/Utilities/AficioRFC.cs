7c95cf086cc1be1d92432ce61c5a9961a193672b
Don Guiruela <guirueladon@gmail.com>
2021-07-22 09:35:25 +0800

    Update AficioRFC.cs

--- Aficio/Utilities/AficioRFC.cs
+++ Aficio/Utilities/AficioRFC.cs
@@ -14,5 +14,5 @@ public class RFC_AficioVersion                            {       //Rail Fence C
         for (int i = 0; i < n; i++)                       {
           for (int j = 0; j < s.Length; j++)              {
             encodedString += rawCipher[i,j].ToString()    ;}}}
-     return encodedString.Replace("\0", string.Empty)     ;}}}
+     return encodedString                                 ;}}}
      
